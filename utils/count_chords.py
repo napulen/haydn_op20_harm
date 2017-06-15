bd7123781e23aff34e9824aab9a08a66e7859f3f
Nestor Napoles <napulen@gmail.com>
2017-06-15 05:07:20 -0500

    Adding an argparser to count_chords script

--- utils/count_chords.py
+++ utils/count_chords.py
@@ -1,24 +1,30 @@
 import os
+import argparse
 import pprint as pp
 
-cwd = '.'
-chords = []
-total = 0
+def countChords(rootdir):
+	chords = []
+	total = 0
+	for root, subfolder, files in os.walk(rootdir):
+		for f in files:
+			if f.endswith(".hrm"):
+				filepath = str(os.path.join(root,f))
+				#print filepath
+				with open(filepath) as fd:
+					for line in fd.readlines():
+						line = line.split()[0]
+						if line != '.' and '!' not in line and '*' not in line and '=' not in line:
+							chords.append(line)
+				print '{}\t{} chord annotations'.format(filepath,len(chords))
+				pp.pprint(chords)
+				print ''
+				total += len(chords)
+				chords = []
+	print 'The total number of chords in the dataset is {}'.format(total)
+	return total,chords
 
-for root, subfolder, files in os.walk(cwd):
-	for f in files:
-		if f.endswith(".hrm"):
-			filepath = str(os.path.join(root,f))
-			#print filepath
-			with open(filepath) as fd:
-				for line in fd.readlines():
-					line = line.split()[0]
-					if line != '.' and '!' not in line and '*' not in line and '=' not in line:
-						chords.append(line)
-			print '{}\t{} chord annotations'.format(filepath,len(chords))
-			pp.pprint(chords)
-			print ''
-			total += len(chords)
-			chords = []
-
-print 'The total number of chords in the dataset is {}'.format(total)						
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Count the number of chords in the corpus')
+    parser.add_argument('-d','--rootdir', metavar='directory', help='Specify the directory of the corpus', default='../op20')
+    args = parser.parse_args()
+    countChords(args.rootdir)					
